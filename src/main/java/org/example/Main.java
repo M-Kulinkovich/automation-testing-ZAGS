1da5289cfcc0b3e72fcc55c2aff3500a90e88fcb
M-kulinkovich <wait_z@mail.ru>
2024-12-02 22:05:29 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
+//        Driver connection
         WebDriver driver = new ChromeDriver();
 
         try {
